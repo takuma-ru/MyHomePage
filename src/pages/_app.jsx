7a03cd1dcd02c531c44ac9c5c8822c0f4bec75c2
takuma-ru <kcattdev@gmail.com>
2021-02-24 22:44:50 +0900

    Add About.jsx contents

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -9,6 +9,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200;300;400;500;600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@100;300;400;500&display=swap" rel="stylesheet" />
         <link rel="shortcut icon" href="/NK_v6.png"></link>
       </Head>
       <Component {...pageProps} />
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -16,7 +16,23 @@ export default function About() {
         <SvgLoader width="" height="45%" className={styles.page_point_2} path="/assets/point.svg" />
       </div>
       <div className={styles.page_contents}>
-        <p>contents.</p>
+        <div className={styles.page_contents_title_l}>
+          <SvgLoader className={styles.title_hex} width="80%" height="80%" path="/assets/item_hex_outline.svg">
+            <span>自己紹介</span>
+          </SvgLoader>
+        </div>
+        <div className={styles.page_contents_text_l}>
+          <span>本文</span>
+        </div>
+
+        <div className={styles.page_contents_text_r}>
+          <span>本文</span>
+        </div>
+        <div className={styles.page_contents_title_r}>
+          <SvgLoader className={styles.title_hex} width="80%" height="80%" path="/assets/item_hex_outline.svg">
+            <span>自己紹介</span>
+          </SvgLoader>
+        </div>
       </div>
     </div>
   )
