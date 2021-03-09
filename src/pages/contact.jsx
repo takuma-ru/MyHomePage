8c291a82aa3ec8f949cd2c26c21013e6fc5e78c3
takuma-ru <kcattdev@gmail.com>
2021-03-09 17:26:58 +0900

    Update page title UI

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -10,9 +10,10 @@ export default function Contact() {
       </Head>
 
       <div  className={styles.page_title}>
-        <SvgLoader width="" height="45%" className={styles.page_point_1} path="/assets/point.svg" />
+        <SvgLoader height="85%" className={styles.title_underbar} path="/assets/title_underbar.svg">
+          <SvgProxy selector="g" stroke-width="1px"></SvgProxy>
+        </SvgLoader>
           CONTACT
-        <SvgLoader width="" height="45%" className={styles.page_point_2} path="/assets/point.svg" />
       </div>
       <div className={styles.page_contents}>
         <p>contents.</p>
--- src/pages/game.jsx
+++ src/pages/game.jsx
@@ -10,9 +10,10 @@ export default function Game() {
       </Head>
 
       <div  className={styles.page_title}>
-        <SvgLoader width="" height="45%" className={styles.page_point_1} path="/assets/point.svg" />
+        <SvgLoader height="65%" className={styles.title_underbar} path="/assets/title_underbar.svg">
+          <SvgProxy selector="g" stroke-width="1px"></SvgProxy>
+        </SvgLoader>
           GAME
-        <SvgLoader width="" height="45%" className={styles.page_point_2} path="/assets/point.svg" />
       </div>
       <div className={styles.page_contents}>
         <p>contents.</p>
