bad7e1cb86208ac690ce058b71bc726d0578b0c7
takuma-ru <kcattdev@gmail.com>
2021-03-18 15:55:26 +0900

    Change favicon

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200;300;400;500;600&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@100;300;400;500&display=swap" rel="stylesheet" />
-        <link rel="shortcut icon" href="/NK_v6.png"></link>
+        <link rel="shortcut icon" href="/NK_v4_5.png"></link>
       </Head>
       <Component {...pageProps} />
     </Layout>
